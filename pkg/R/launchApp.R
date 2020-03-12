3b91da30358247c508ce90f553265356f99a5cb7
jarathomas <jarathomas@gmail.com>
2020-03-12 00:15:31 -0400

    increased allowable size of upload file

--- pkg/R/launchApp.R
+++ pkg/R/launchApp.R
@@ -4,9 +4,14 @@
 #'
 #' @examples
 #' \dontrun{
-#' library(shinyVA)
+#' library(openVAapp)
 #' launchApp()
 #' }
 launchApp <- function() {
+  origMaxRS <- getOption("shiny.maxRequestSize")
+  origWidth <- getOption("width")
+  old <- options(shiny.maxRequestSize = origMaxRS, width = origWidth)
+  options(shiny.maxRequestSize = 100*1024^2, width = 100)
   shiny::runApp(appDir = system.file('app', package = 'openVAapp'))
+  on.exit(options(old), add = TRUE)
 }
